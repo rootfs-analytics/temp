01d45c0e1c35a4654c1a4e14a287c6c42ea2facb
h <mmttdebbcc@yahoo.com>
2014-11-30 21:40:17 -0500

    add asymm function
    Signed-off-by: h <mmttdebbcc@yahoo.com>

--- break.r
+++ break.r
@@ -17,5 +17,22 @@ bo <- function (x, y, len = 1000, size = 30)
    bbb = tail (bb, l)
    c = aaa - bbb
    res = breakout(c, min.size=size, method='multi', beta=.001, degree=1, plot=TRUE)
-   res
+   return (res)
+}
+
+wbo <- function (x, y = "SPY", len = 500, size = 30, rsize = 10)
+{
+   a = tail(getSymbols(x,auto.assign=FALSE),len)
+   ar = dailyReturn(a)
+   #b = tail(getSymbols(y,auto.assign=FALSE),len)
+   #br = dailyReturn(b)
+   c = as.vector(ar)#/as.vector(br)
+   #print(c)
+   res = breakout(c, min.size=size, min.rsize = rsize, method='multi', beta=.001, degree=0, plot=FALSE)
+   plot(a,type='candles',main = x)
+   points(Cl(a)[res$loc+1],col="red")
+   #points(Cl(a)[res$loc - res$start], col = "blue")
+   #points(Cl(a)[res$loc + res$end], col = "green")
+ 
+   return (res)
 }
